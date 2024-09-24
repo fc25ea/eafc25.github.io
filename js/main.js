24a57e6eca20a8042a45afd5d98c9aa225a71c37
fc25ea <ardagulwerk@gmail.com>
2024-09-24 10:13:42 +0200

    js

---
+++ js/main.js
@@ -0,0 +1,13 @@
+console.log('Succes!')
+
+const inkoopInput = document.querySelector('inkoop');
+const verkoopInput = document.querySelector('verkoop');
+
+function berekenBtn() {
+    if (inkoopInput.value == 0) {
+        alert('0')
+    } else {
+        alert('25')
+    }
+}
+
